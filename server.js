6cf8b329b767c2674f40c5b2be976f3c990f20e6
Teguh Mad Solekhan <teguhmad123@gmail.com>
2023-10-18 18:21:40 +0700

    feat(server):update header and not found url

--- server.js
+++ server.js
@@ -1,28 +1,35 @@
 const http = require('http');
      
 const requestListener = (request, response) => {
-    response.setHeader('Content-Type', 'text/html');
+    response.setHeader('Content-Type', 'application/json');
+    response.setHeader('X-Powered-By', 'NodeJS');
 
-    response.statusCode = 200;
- 
+    
     const { url, method } = request;
- 
+    
     if(url === '/books'){
         if(method === 'GET') {
+            response.statusCode = 200;
             response.end('<h1>GET</h1>');
         }
     
         if(method === 'POST') {
+            response.statusCode = 200;
             response.end('<h1>POST</h1>');
         }
     
         if(method === 'PUT') {
+            response.statusCode = 200;
             response.end('<h1>PUT</h1>');
         }
     
         if(method === 'DELETE') {
+            response.statusCode = 200;
             response.end('<h1>DELETE</h1>');
         }
+    }else{
+        response.statusCode = 404;
+        response.end('<h1>Not Found!</h1>')
     }
 };
  
