029267c0fbd4e205918b428a748fdb512c131683
Teguh Mad Solekhan <teguhmad123@gmail.com>
2023-10-18 18:18:11 +0700

    feat(server): detect url and metode

--- server.js
+++ server.js
@@ -1,13 +1,30 @@
 const http = require('http');
-
-
+     
 const requestListener = (request, response) => {
     response.setHeader('Content-Type', 'text/html');
- 
+
     response.statusCode = 200;
-    response.end('<h1>Halo HTTP Server!</h1>');
-};
  
+    const { url, method } = request;
+ 
+    if(url === '/books'){
+        if(method === 'GET') {
+            response.end('<h1>GET</h1>');
+        }
+    
+        if(method === 'POST') {
+            response.end('<h1>POST</h1>');
+        }
+    
+        if(method === 'PUT') {
+            response.end('<h1>PUT</h1>');
+        }
+    
+        if(method === 'DELETE') {
+            response.end('<h1>DELETE</h1>');
+        }
+    }
+};
  
 const server = http.createServer(requestListener);
  
