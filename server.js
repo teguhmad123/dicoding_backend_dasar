0bc8d8317cab90ba80e96b70b2e929edcb27c28a
Teguh <teguhmad123@gmail.com>
2023-10-19 14:18:35 +0700

    show json response

--- server.js
+++ server.js
@@ -1,43 +1,62 @@
-const http = require('http');
-     
-const requestListener = (request, response) => {
-    response.setHeader('Content-Type', 'application/json');
-    response.setHeader('X-Powered-By', 'NodeJS');
+const http = require("http");
 
-    
-    const { url, method } = request;
-    
-    if(url === '/books'){
-        if(method === 'GET') {
-            response.statusCode = 200;
-            response.end('<h1>GET</h1>');
-        }
-    
-        if(method === 'POST') {
-            response.statusCode = 200;
-            response.end('<h1>POST</h1>');
-        }
-    
-        if(method === 'PUT') {
-            response.statusCode = 200;
-            response.end('<h1>PUT</h1>');
-        }
-    
-        if(method === 'DELETE') {
-            response.statusCode = 200;
-            response.end('<h1>DELETE</h1>');
-        }
-    }else{
-        response.statusCode = 404;
-        response.end('<h1>Not Found!</h1>')
+const requestListener = (request, response) => {
+  response.setHeader("Content-Type", "application/json");
+  response.setHeader("X-Powered-By", "NodeJS");
+
+  const { url, method } = request;
+
+  if (url === "/books") {
+    if (method === "GET") {
+      response.statusCode = 200;
+      response.end(
+        JSON.stringify({
+          message: "GET",
+        })
+      );
     }
+
+    if (method === "POST") {
+      response.statusCode = 200;
+      response.end(
+        JSON.stringify({
+          message: "POST",
+        })
+      );
+    }
+
+    if (method === "PUT") {
+      response.statusCode = 200;
+      response.end(
+        JSON.stringify({
+          message: "PUT",
+        })
+      );
+    }
+
+    if (method === "DELETE") {
+      response.statusCode = 200;
+      response.end(
+        JSON.stringify({
+          message: "DELETE",
+        })
+      );
+    }
+  } else {
+    response.statusCode = 404;
+    response.end(
+      JSON.stringify({
+        message: "NOT FOUND!",
+      })
+    );
+  }
 };
- 
+
 const server = http.createServer(requestListener);
- 
+
 const port = 9000;
-const host = 'localhost';
- 
+const host = "localhost";
+
 server.listen(port, host, () => {
-    console.log(`Server berjalan pada http://${host}:${port}`);
+  console.log(`Server berjalan pada http://${host}:${port}`);
 });
